0de974dc0deae6fe7d89a34c970c6fb73811b6d5
Roman Surinov <romasurinov13@mail.ru>
2021-01-09 23:27:12 +0300

    test apk

---
+++ Stars/Assets/Scripts/ButtonHub.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class ButtonHub : MonoBehaviour
+{
+  
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+        
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("Hub");
+    }
+
+    public void Some()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Something()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
---
+++ Stars/Assets/Scripts/Click.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Click : MonoBehaviour
+{
+    public int count;
+    public Text textLayer;
+    void Start()
+    {
+        count = 0;
+        textLayer.text = count.ToString();
+
+    }
+
+    void Update()
+    {
+        textLayer.text = count.ToString();
+    }
+
+    public void Inc()
+    {
+        count++;
+    }
+}
